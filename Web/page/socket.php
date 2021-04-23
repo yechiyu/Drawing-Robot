7cfb0904abc67357f077f03efb020fccfc40f648
Qikai Yang <942558965@qq.com>
2021-04-23 21:10:13 +0100

    update page for socket

--- Web/page/socket.php
+++ Web/page/socket.php
@@ -7,7 +7,7 @@ $ip = "127.0.0.1";
 
 $fileName = $_FILES['photo']['name'];
 $file = $_FILES['photo']['tmp_name'];
-$path = "../storeImg";
+$path = "../storeImg/";
 move_uploaded_file($file, $path . $fileName);
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -61,7 +61,7 @@ if ($result < 0) {
   echo "connect OK<br>";
 }
 
-$sideValueLaV = $_POST['sideValueLaV'];
+$sideValueLaV = "la".$_POST['sideValueLaV'];
 $out = '';
 
 if (!socket_write($socket, $sideValueLaV, strlen($sideValueLaV))) {
@@ -100,7 +100,7 @@ if ($result < 0) {
   echo "connect OK<br>";
 }
 
-$sideValueLbV = $_POST['sideValueLbV'];
+$sideValueLbV = "lb".$_POST['sideValueLbV'];
 $out = '';
 
 if (!socket_write($socket, $sideValueLbV, strlen($sideValueLbV))) {
@@ -108,6 +108,45 @@ if (!socket_write($socket, $sideValueLbV, strlen($sideValueLbV))) {
 } else {
   echo "Message sent to server successful！\n";
   echo "The content to be sent is: Lb = $sideValueLbV ";
+}
+
+while($out = socket_read($socket, 8192)) {
+  echo "The receiving server returned the message successfully！<br>";
+  echo "The accepted content is:", $out;
+}
+
+socket_close($socket);
+
+
+error_reporting(E_ALL);
+set_time_limit(0);
+
+$port = 8080;
+$ip = "127.0.0.1";
+
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket < 0) {
+  echo "socket_create() failed: reason: " . socket_strerror($socket) . "\n";
+} else {
+  echo "OK.\n";
+}
+
+$result = socket_connect($socket, $ip, $port);
+if ($result < 0) {
+  echo "socket_connect() failed.\nReason: ($result) " . socket_strerror($result) . "\n";
+} else {
+  echo "connect OK<br>";
+}
+
+$drawingit = "drawingit";
+$out = '';
+
+if (!socket_write($socket, $drawingit, strlen($drawingit))) {
+  echo "socket_write() failed: reason: " . socket_strerror($socket) . "\n";
+} else {
+  echo "Message sent to server successful！\n";
+  echo "The content to be sent is: $drawingit ";
 }
 
 while($out = socket_read($socket, 8192)) {
